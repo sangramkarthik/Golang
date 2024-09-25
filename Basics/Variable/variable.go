34e6e450e1454251990885baaac04f589b55a647
sangramkarthik <sangramkarthik@outlook.com>
2024-09-25 23:33:05 +0530

    variables in go

---
+++ Basics/Variable/variable.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var name string
+
+	var age int
+
+	name = "sangram"
+
+	age = 21
+
+	pi := 3.14178
+
+	var product = 100
+
+	greeting := "Namaste"
+
+	fmt.Printf("%v My Name is %v\n",greeting+",",name+".")
+	fmt.Printf("I'm %v years old\n",age)
+	fmt.Printf("The value of Pi is %v\n",pi)
+	fmt.Printf("The Product of 10 * 10 is %v \n",product)
+
+}
