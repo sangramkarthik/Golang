9ab82bf6b1e7a152fac9ee41c53c5aa3cdc26fc4
sangramkarthik <sangramkarthik@outlook.com>
2024-09-25 23:42:53 +0530

    constants in go

---
+++ Basics/Constant/constants.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const name ="sangram"
+
+func main(){
+
+	const pi = 3.14178
+
+	const (
+		age = 21
+
+		interest = "Boxing"
+
+	)
+
+	fmt.Printf("Hi!,I'm %v. I'm %v.\n",name,age)
+	fmt.Printf("I like %v\n",interest)
+	fmt.Printf("The value of Pi is %v \n",pi)
+}
